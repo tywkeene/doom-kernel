5159192ae538f77cdcdd1252b597d933f5214bf7
Tyrell Keene <tyrell.keene@contrastsecurity.com>
2021-03-29 17:26:24 -0600

    update scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -I./include/ -std=gnu99 -c kernel/kernel.c -o kernel.o -O2 -Wall -Wextra
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -I./include/ -std=gnu99 -c kernel/mmio.c -o mmio.o -O2 -Wall -Wextra
 arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -I./include/ -std=gnu99 -c kernel/uart.c -o uart.o -O2 -Wall -Wextra
 arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -I./include/ -std=gnu99 -c kernel/printk.c -o printk.o -O2 -Wall -Wextra
-arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c boot/boot.s -o boot.o
-arm-none-eabi-gcc -T boot/linker.ld -ffreestanding -O2 -nostdlib -o doomos.elf *.o -lgcc
+
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c asm/boot.s -o boot.o
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c asm/utils.s -o utils.o
+
+arm-none-eabi-gcc -T linker.ld -ffreestanding -O2 -nostdlib -o doomos.elf *.o -lgcc
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-qemu-system-arm -m 1024 -M raspi2 -serial stdio -kernel doomos.elf
+qemu-system-arm -m 1024 -smp 4 -M raspi2 -serial stdio -kernel doomos.elf
